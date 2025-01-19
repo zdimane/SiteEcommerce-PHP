881223c91def7d39b252e31fbe5299c45d934543
IMANE ZAAD <104379762+zdimane@users.noreply.github.com>
2025-01-19 22:14:13 +0100

    Update bd.php

--- BD/bd.php
+++ BD/bd.php
@@ -1,7 +1,8 @@
 <?php
 //$con=mysqli_connect("localhost:8278","root","","ecommerce_cms");
 //$con = mysqli_connect('HOSTNAME','USERNAME','PASSWORD');
-$conn = new mysqli("localhost", "root", "", "ecommerce_cms","3307");
+$password = getenv('MYSQL_SECURE_PASSWORD');
+$conn = new mysqli($servername, $username, $password);
 if(mysqli_connect_error())
 {
     echo"Failed to connect to mysql : ".mysqli_connect_error();
